8916c1dfa1c018ea089be4ea179fb4c43ed3a6ec
nate o <nateo@nates-MBP.fios-router.home>
2022-06-23 10:22:14 -0400

    basics

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-let new = 1;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000 
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
