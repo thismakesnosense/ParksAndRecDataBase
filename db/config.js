c976660187f954b72e2bed77fd76a3593726b215
nate o <nateo@nates-MBP.fios-router.home>
2022-07-19 17:33:24 -0400

    env

--- db/config.js
+++ db/config.js
@@ -1,2 +1,18 @@
 const mongoose = require("mongoose");
 
+let connectDB = async () => {
+    try {
+
+
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        })
+        console.log("connected to database");
+    } catch (err) {
+        console.error(err)
+    }
+};
+
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080 
+const dotenv = require('dotenv');
+const connectDB = require('./db/config');
 
+dotenv.config();
+connectDB();
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
